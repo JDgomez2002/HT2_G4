d2a2c7f66a3c1e1acc3bba5331a13a8985862c39
JDgomez2002 <gom21429@uvg.edu.gt>
2022-01-25 12:43:17 -0600

    Commit inicial

---
+++ MainG4.java
@@ -0,0 +1,15 @@
+//Universidad del Valle de Guatemala
+//Algoritmos y Estructura de Datos
+//Catedrático Douglas Barrios
+//Primer Semestre 2022
+//José Daniel Gómez Cabrera 21429
+//Jorge Fernando Adrino Menéndez 21616
+//Juan Pablo Cordón Cotero 21458
+//Sección 41
+//Actividad: HT2
+
+public class MainG4{
+    public static void main(String[] args) {
+        
+    }
+}
